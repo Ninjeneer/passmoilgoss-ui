404c03560b5c2c6dd9992950e4863acaf86a3fe4
Loan ALOUACHE <alouache.loan@gmail.com>
2022-01-13 14:31:58 +0100

    feat: config file

--- src/app/rest.service.ts
+++ src/app/rest.service.ts
@@ -3,23 +3,22 @@ import { HttpClient, HttpResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { Orphan } from "./entities/orphan.entity";
+import config from '../assets/config.json';
 
 @Injectable()
 export default class RestService {
   constructor(private readonly httpClient: HttpClient) { }
 
-  private backend = "http://localhost:3000";
-
   public findAllOrphans(params): Observable<HttpResponse<Orphan[]>> {
-    return this.httpClient.get<Orphan[]>(`${this.backend}/orphans`, { params, observe: 'response' });
+    return this.httpClient.get<Orphan[]>(`${config.host}/orphans`, { params, observe: 'response' });
   }
 
   public findOrphan(id: string): Observable<HttpResponse<Orphan>> {
-    return this.httpClient.get<Orphan>(`${this.backend}/orphans/${id}`, { observe: 'response' });
+    return this.httpClient.get<Orphan>(`${config.host}/orphans/${id}`, { observe: 'response' });
   }
 
   public findCountries(): Observable<HttpResponse<string[]>> {
-    return this.httpClient.get<string[]>(`${this.backend}/orphans/countries`, { observe: 'response' });
+    return this.httpClient.get<string[]>(`${config.host}/orphans/countries`, { observe: 'response' });
   }
 
 }
